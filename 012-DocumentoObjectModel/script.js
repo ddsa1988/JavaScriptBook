c231acf64eb62b7257b99995d7b15525ab14035d
Diego Alexandre <ddsa.1988@outlook.com>
2022-10-31 19:10:11 -0300

    commit

---
+++ 012-DocumentoObjectModel/script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const h1 = document.querySelector("h1");
+h1.style.backgroundColor = "red";
+window.alert(h1.textContent);
+console.log(`The title of this document is: ${document.title}.`);
+console.log(`The URL of this document is: ${document.URL}`);
