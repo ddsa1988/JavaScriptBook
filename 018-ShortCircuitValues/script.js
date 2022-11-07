518c4f96c453f3fbf0070a62ea80c8593a630c40
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-07 00:01:29 -0300

    commit

---
+++ 018-ShortCircuitValues/script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("Diego || false:", "Diego" || false);
+console.log("false || Diego:", false || "Diego");
+console.log("Diego && false:", "Diego" && false);
+console.log("false && Diego:", false && "Diego");
