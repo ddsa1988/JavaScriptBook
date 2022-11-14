7e536f23bb7a126bf73ce6882d9002997ffbb547
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-13 22:32:47 -0300

    commit

---
+++ 021-PreviousAndNextSibling/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const startItem = document.getElementById("two");
+console.log(startItem);
+console.log(startItem.parentElement.previousSibling);
+console.log(startItem.nextSibling);
---
+++ 022-FirstAndLastChild/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const startItem = document.getElementsByTagName("ul")[0];
+console.log(document.getElementsByTagName("ul"));
+console.log(startItem);
+console.log(startItem.firstChild);
+console.log(startItem.lastChild);
