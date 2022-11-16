5f91299956dbe4822c801652a6559aeb98dfb7d4
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-15 23:51:21 -0300

    commit

---
+++ 023-AccessAndUpdateTextNode/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log(document.getElementById("one"));
+console.log(document.getElementById("one").firstChild);
+console.log(document.getElementById("one").lastChild);
+//console.log(document.getElementById("one").firstChild.nextSibling);
+console.log(document.getElementById("one").firstChild.nextSibling.nodeValue);
+//console.log(document.getElementById("one").lastChild.previousSibling);
---
+++ 025-AccessAndUpdateTextWithTextContent/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const itemOne = document.getElementById("one");
+const itemOneTextContent = itemOne.textContent;
+
+console.log(itemOneTextContent);
+
+itemOne.textContent = "Cheese";
