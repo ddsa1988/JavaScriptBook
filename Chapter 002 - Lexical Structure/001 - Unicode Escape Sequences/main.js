cd5f0d6ec45d7ca3b520438cb463fcc5d46984ac
Diego Alexandre <ddsa.1988@outlook.com>
2022-12-09 07:31:13 -0300

    commit

---
+++ Chapter 002 - Lexical Structure/001 - Unicode Escape Sequences/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const café = 10; //Define a variable using a Unicode character
+console.log(café);
+console.log(café); //Access the variable using an escape sequence
+console.log(café); //Another form of the same escape sequence
+console.log("\u{1F600}"); //Prints a smiley face emoji
