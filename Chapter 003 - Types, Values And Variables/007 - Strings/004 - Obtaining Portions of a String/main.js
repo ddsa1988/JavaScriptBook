356370ee444729a9d2facb8b631b6f5f7468b45f
Diego Alexandre <ddsa.1988@outlook.com>
2022-12-12 21:37:19 -0300

    commit

--- Chapter 003 - Types, Values And Variables/007 - Strings/004 - Obtaining Portions of a String/main.js
+++ Chapter 003 - Types, Values And Variables/007 - Strings/004 - Obtaining Portions of a String/main.js
@@ -1,3 +1,22 @@
 "use strict";
 
-const msg = "Hellow World!";
+let msg1 = "Hellow World!";
+let msg2 = msg1.substring(1, 5);
+console.log("Substring method:");
+console.log(msg1);
+console.log(msg2);
+console.log();
+
+msg1 = "Hellow World!";
+msg2 = msg1.slice(1, 5);
+console.log("Slice method:");
+console.log(msg1);
+console.log(msg2);
+console.log();
+
+msg1 = "Hellow World!";
+msg2 = msg1.split(" ");
+console.log("Split method:");
+console.log(msg1);
+console.log(msg2);
+console.log();
---
+++ Chapter 003 - Types, Values And Variables/007 - Strings/005 - Searching a String/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const msg1 = "Hellow World!";
+
+console.log(msg1.indexOf("l")); //Return the first index of the substring in the string
+console.log(msg1.indexOf("l", 3)); //Return the first index of the substring in the string starting from the index 3
+console.log(msg1.indexOf("a")); //Return -1 is when doesn't find the substring in the string
+console.log(msg1.lastIndexOf("l")); //Return the last index of the substring in the string
+console.log();
+
+function countChar(msg, char) {
+    if (typeof msg !== "string") throw TypeError("Parameter must be a string");
+    if (typeof char !== "string" || char.length !== 1)
+        throw TypeError("Parameter must be a char with length of 1");
+
+    let index = 0;
+    let count = 0;
+
+    for (let i = 0; i < msg.length; i++) {
+        if (msg.indexOf(char, index) > -1) {
+            index = msg.indexOf(char, index) + 1;
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(countChar(msg1, "l"));
+console.log(countChar(msg1, "o"));
