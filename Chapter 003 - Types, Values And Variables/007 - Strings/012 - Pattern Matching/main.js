772bfbd1903cb26500f7d400415479b71cf23813
Diego Alexandre <ddsa.1988@outlook.com>
2022-12-13 07:45:10 -0300

    commit

--- Chapter 003 - Types, Values And Variables/007 - Strings/012 - Pattern Matching/main.js
+++ Chapter 003 - Types, Values And Variables/007 - Strings/012 - Pattern Matching/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// /^HTML/ = Match the letters HTML at the start of a string
+
+// /[1-9][0-9]*/ = Match a nonzero digit, followedd by any # of digits
+
+// /\bjavascript\b/i = Match "javascript" as a word, caseinsensitive
+
+const text = "testing: 1, 2, 3";
+const pattern = /\d+/g;
+
+console.log(pattern.test(text)); //Return true if finds a match
+console.log(text.search(pattern)); //Returns the position of the first match
+console.log(text.match(pattern)); //Return an array with all matches
+console.log(text.replace(pattern, "#"));
+console.log(text.split(/\D+/)); //Split on nondigits
