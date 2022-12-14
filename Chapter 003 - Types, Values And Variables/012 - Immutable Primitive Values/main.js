83b62e150a267ac91e2b0f32e04efad977c944de
Diego Alexandre <ddsa.1988@outlook.com>
2022-12-14 12:47:42 -0300

    commit

--- Chapter 003 - Types, Values And Variables/012 - Immutable Primitive Values/main.js
+++ Chapter 003 - Types, Values And Variables/012 - Immutable Primitive Values/main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+/*
+Primitive types: undefined, null, booleans, numbers and strings.
+Primitive types are immutable. You cannot change their values. You compare them by value.
+*/
+
+const number1 = 200;
+const number2 = 200;
+
+const string1 = "diego";
+const string2 = " diego";
+
+console.log(number1 === number2);
+console.log(string1 === string2);
+console.log(string1.toUpperCase()); //Returns a new string and the original remains unchanged
+console.log(string1);
--- Chapter 003 - Types, Values And Variables/013 - Mutable Object References/main.js
+++ Chapter 003 - Types, Values And Variables/013 - Mutable Object References/main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+/*
+Reference types: object and array.
+Reference types are mutable. You can change their values and size. You compare them by refernce.
+*/
+
+const myArray1 = [1, 2, 3];
+const myArray2 = [1, 2, 3];
+
+console.log(myArray1 === myArray2); //False becasue the two variables points to diferrent references
+
+const a = [1, 2, 3];
+const b = a;
+
+console.log(a === b); //True becasue the two variables points to the same reference
+
+a.push(250);
+console.log(a);
+console.log(b);
