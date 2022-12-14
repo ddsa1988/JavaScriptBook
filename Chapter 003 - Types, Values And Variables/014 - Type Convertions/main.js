9ddc424aee01d83208b2c1bb9d2561edd97877c2
Diego Alexandre <ddsa.1988@outlook.com>
2022-12-14 13:13:57 -0300

    commit

---
+++ Chapter 003 - Types, Values And Variables/014 - Type Convertions/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log(10 + " diego");
+console.log("2" * " 10");
+console.log(1 - "x");
+console.log();
+
+console.log(Number.parseInt("12-34"));
+console.log(Number("12-34"));
