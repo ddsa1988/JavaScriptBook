d6cff100890f5e5e02c576163e2e265d19447fb8
Diego Alexandre <ddsa.1988@outlook.com>
2022-12-18 13:10:46 -0300

    commit

--- Chapter 005 - Statements/013 - Loop For In With Objects/main.js
+++ Chapter 005 - Statements/013 - Loop For In With Objects/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+//The for/in statement loops through the property names of a specified object.
+
+const data = {
+    firstName: "Diego",
+    lastName: "Alexandre",
+    birthYear: 1988,
+    age: function (year) {
+        return year - this.birthYear;
+    },
+};
+
+for (const key in data) {
+    console.log(`Key: ${key} - Value: ${data[key]}`);
+}
+console.log();
+
+//Copy the names of all object properties into an array:
+const arr = new Array();
+let i = 0;
+
+for (arr[i++] in data); //Empty
+console.log(arr);
