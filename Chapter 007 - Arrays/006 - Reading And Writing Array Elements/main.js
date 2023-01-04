f26f482a9a7cbc4aa21186d8ca103a178daf3917
Diego Alexandre <ddsa.1988@outlook.com>
2023-01-04 06:55:31 -0300

    commit

--- Chapter 007 - Arrays/006 - Reading And Writing Array Elements/main.js
+++ Chapter 007 - Arrays/006 - Reading And Writing Array Elements/main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const a = ["world"];
+console.log(a);
+console.log(a[0]);
+console.log(a[10]);
+
+a[1] = "Diego";
+console.log(a);
+
+const b = 2;
+a[b] = 250;
+console.log(a);
+console.log(a.length);
+console.log();
+
+const c = ["New York", "Curitiba", "João Pessoa"];
+a.push(c);
+console.log(a);
+console.log();
+
+const d = [...a, ...c];
+console.log(d);
---
+++ Chapter 007 - Arrays/007 - Sparse Arrays/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const a = [1, , 3, ,];
+console.log(a);
+console.log(a[1]);
+console.log(a.length);
