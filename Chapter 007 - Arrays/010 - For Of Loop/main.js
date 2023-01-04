d203479deb549f6ae530e5303246dacc616a6832
Diego Alexandre <ddsa.1988@outlook.com>
2023-01-04 20:11:43 -0300

    commit

--- Chapter 007 - Arrays/010 - For Of Loop/main.js
+++ Chapter 007 - Arrays/010 - For Of Loop/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const letters = [..."Diego", , , ..."dos"];
+console.log(letters);
+console.log();
+
+for (const value of letters) {
+    console.log(value);
+}
+console.log();
+
+for (const [index, value] of letters.entries()) {
+    console.log(index, value);
+}
---
+++ Chapter 007 - Arrays/011 - For Each() Loop/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const letters = [..."Diego", , , ..."dos"];
+console.log(letters);
+console.log();
+
+letters.forEach(function (value) {
+    console.log(value);
+});
