08cad979d7dc1f841f3ef0083c6143842f747de2
Diego Alexandre <ddsa.1988@outlook.com>
2023-01-06 14:19:21 -0300

    commit

--- Chapter 007 - Arrays/021 - Stacks and Queues/main.js
+++ Chapter 007 - Arrays/021 - Stacks and Queues/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+//Stack: First In - Last Out
+const stack = new Array();
+console.log(stack);
+
+for (let i = 0; i < 10; i++) {
+    stack.push(i);
+}
+console.log(stack);
+console.log();
+
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
+console.log();
+
+//Queue: First In - First Out
+const queue = new Array();
+console.log(queue);
+
+for (let i = 10; i > 0; i--) {
+    queue.push(i);
+}
+console.log(queue);
+console.log(queue.shift());
+console.log(queue);
+console.log(queue.shift());
+console.log(queue);
